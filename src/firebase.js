960f93fea8650a7ec9481d0cf52b8ea0b3f3d350
Rabin-2004 <rpoudyal2004@gmail.com>
2024-12-11 16:37:06 +0545

    database created

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc, getDoc } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDaN7XbxQNPhIAAQRtAEP8w-gEHoTE0-Zk",
@@ -11,7 +13,43 @@ const firebaseConfig = {
   measurementId: "G-EQ7N8F2KR9"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); 
 
-export { auth }; 
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app);
+
+
+const db = getFirestore(app);
+
+const saveUserProfile = async (userId, score, accuracy) => {
+  try {
+    const userRef = doc(db, "users", userId); 
+    await setDoc(userRef, {
+      score: score,
+      accuracy: accuracy,
+    }, { merge: true });  
+
+    console.log("User profile saved/updated!");
+  } catch (error) {
+    console.error("Error saving user profile: ", error);
+  }
+};
+
+const getUserProfile = async (userId) => {
+  try {
+    const userRef = doc(db, "users", userId); 
+    const docSnap = await getDoc(userRef);
+
+    if (docSnap.exists()) {
+      console.log("User Profile:", docSnap.data()); 
+      return docSnap.data();
+    } else {
+      console.log("No such document!");
+    }
+  } catch (error) {
+    console.error("Error getting user profile: ", error);
+  }
+};
+
+export { auth, db, saveUserProfile, getUserProfile };
