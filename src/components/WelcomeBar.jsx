1564d460f9c47a31048cf3105abfb3c9f17c8605
Rabin-2004 <rpoudyal2004@gmail.com>
2024-12-11 16:21:39 +0545

    fixed a bug where welcome message was showing multiple times

--- src/components/WelcomeBar.jsx
+++ src/components/WelcomeBar.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearch } from "../context/SearchContext";
 import { Snackbar, Alert } from "@mui/material";
 
 const Welcomebar = () => {
 
 const {isLoggedIn, displayName} = useSearch();
-const [openSnackbar, setOpenSnackBar] = useState(true);
+const [openSnackbar, setOpenSnackBar] = useState(false);
+
+useEffect(()=>{
+  if (isLoggedIn && sessionStorage.getItem('welcomeShown')){
+    setOpenSnackBar(true);
+    sessionStorage.setItem('welcomeShown', 'true')
+  }
+}, [isLoggedIn])
 
 function handleClose(){
 setOpenSnackBar(t => !t)
@@ -14,7 +21,7 @@ setOpenSnackBar(t => !t)
   return (
     <>
     {
-        isLoggedIn && (
+        isLoggedIn && openSnackbar && (
             <Snackbar open={openSnackbar}
             autoHideDuration = {3000}
             onClose = {handleClose}
